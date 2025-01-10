e11564e508113ef775752e161f5d1e09bed0def7
Bouwe Andela <b.andela@esciencecenter.nl>
2025-01-10 17:14:00 +0100

    Add forgotten file

---
+++ packages/ref-metrics-esmvaltool/src/ref_metrics_esmvaltool/metrics/__init__.py
@@ -0,0 +1,9 @@
+"""ESMValTool metrics."""
+
+from ref_metrics_esmvaltool.metrics.ecs import EquilibriumClimateSensitivity
+from ref_metrics_esmvaltool.metrics.example import GlobalMeanTimeseries
+
+__all__ = [
+    "EquilibriumClimateSensitivity",
+    "GlobalMeanTimeseries",
+]
